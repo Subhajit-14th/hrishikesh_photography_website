c000b70862a62bda86cae3bb99de43035c50a241
Subhajit-14th <subhajitdtt700@gmail.com>
2025-06-23 01:46:02 +0530

    header part is responsive for all the screen(the last responsive dimension is 278)

---
+++ src/component/enquire/enquire.js
@@ -0,0 +1,7 @@
+import bimg1 from "../../assets/headerBanner/imageOne.jpg";
+
+function Enquire() {
+  return <div className="enquire"></div>;
+}
+
+export default Enquire;
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,27 +1,25 @@
 import "./header.css";
+
 function Header() {
   return (
     <header className="header">
+      <div className="logo">
+        <p className="logo-heading">WEDDING PHOTO STATION</p>
+        <p className="sub-logo-name">HRISIKESH CHAKRABORTY</p>
+      </div>
+
       <nav className="navbar">
-        <ul className="nav-left">
+        <ul className="nav-menu">
           <li>
             <a href="/home">Home</a>
           </li>
           <li>
+            <a href="/galleries">Galleries</a>
+          </li>
+          <li>
             <a href="/stories">Stories</a>
           </li>
-        </ul>
-
-        <div className="logo">
-          {/* <img src={logo} alt="Logo" /> */}
-          <p className="logo-heading">WEDDING PHOTO STATION</p>
-          <p className="sub-logo-name">HRISIKESH CHAKRABORTY</p>
-        </div>
-
-        <ul className="nav-right">
-          <li>
-            <a href="/testimonials">Film</a>
-          </li>
+          {/* <li><a href="/testimonials">Testimonials</a></li> */}
           <li>
             <a href="/about">About</a>
           </li>
@@ -33,4 +31,5 @@ function Header() {
     </header>
   );
 }
+
 export default Header;
